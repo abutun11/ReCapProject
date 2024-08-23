ff256bba4e5d3369ca77ed99961f67896480266a
abutun11 <butunalican01@gmail.com>
2024-08-23 20:02:37 +0300

    23.08.2024 - 11. Ders 1, 2 ve 3. Ödev Kodları

--- WebAPI/Program.cs
+++ WebAPI/Program.cs
@@ -1,6 +1,9 @@
 
+using Autofac.Extensions.DependencyInjection;
+using Autofac;
 using Business.Abstract;
 using Business.Concrete;
+using Business.DependencyResolvers.Autofac;
 using DataAccess.Abstract;
 using DataAccess.Concrete.EntityFramework;
 
@@ -12,21 +15,26 @@ namespace WebAPI
         {
             var builder = WebApplication.CreateBuilder(args);
 
+            builder.Host.UseServiceProviderFactory(new AutofacServiceProviderFactory()).ConfigureContainer<ContainerBuilder>(builder =>
+            {
+                builder.RegisterModule(new AutofacBusinessModule());
+            });
+
             // Add services to the container.
 
             builder.Services.AddControllers();
-            builder.Services.AddSingleton<IUserService, UserManager>();
-            builder.Services.AddSingleton<IUserDal, EfUserDal>();
-            builder.Services.AddSingleton<IRentalService, RentalManager>();
-            builder.Services.AddSingleton<IRentalDal, EfRentalDal>();
-            builder.Services.AddSingleton<ICustomerService, CustomerManager>();
-            builder.Services.AddSingleton<ICustomerDal, EfCustomerDal>();
-            builder.Services.AddSingleton<IColorService, ColorManager>();
-            builder.Services.AddSingleton<IColorDal, EfColorDal>();
-            builder.Services.AddSingleton<ICarService, CarManager>();
-            builder.Services.AddSingleton<ICarDal, EfCarDal>();
-            builder.Services.AddSingleton<IBrandService, BrandManager>();
-            builder.Services.AddSingleton<IBrandDal, EfBrandDal>();
+            //builder.Services.AddSingleton<IUserService, UserManager>();
+            //builder.Services.AddSingleton<IUserDal, EfUserDal>();
+            //builder.Services.AddSingleton<IRentalService, RentalManager>();
+            //builder.Services.AddSingleton<IRentalDal, EfRentalDal>();
+            //builder.Services.AddSingleton<ICustomerService, CustomerManager>();
+            //builder.Services.AddSingleton<ICustomerDal, EfCustomerDal>();
+            //builder.Services.AddSingleton<IColorService, ColorManager>();
+            //builder.Services.AddSingleton<IColorDal, EfColorDal>();
+            //builder.Services.AddSingleton<ICarService, CarManager>();
+            //builder.Services.AddSingleton<ICarDal, EfCarDal>();
+            //builder.Services.AddSingleton<IBrandService, BrandManager>();
+            //builder.Services.AddSingleton<IBrandDal, EfBrandDal>();
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
